af37b9b038cde0fb78a995c0d12844769837e770
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-02-23 17:32:29 +0100

    tl;dr

--- plist.go
+++ plist.go
@@ -5,28 +5,28 @@ const plist string = `
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-  <key>Label</key>
-  <string>dev.jpedro.device</string>
+    <key>Label</key>
+    <string>dev.jpedro.device</string>
 
-  <key>ProgramArguments</key>
-  <array>
-    <string>device</string>
-    <string>sync</string>
-  </array>
+    <key>ProgramArguments</key>
+    <array>
+        <string>device</string>
+        <string>sync</string>
+    </array>
 
-  <key>Nice</key>
-  <integer>1</integer>
+    <key>Nice</key>
+    <integer>1</integer>
 
-  <key>StartInterval</key>
-  <integer>10</integer>
+    <key>StartInterval</key>
+    <integer>10</integer>
 
-  <key>RunAtLoad</key>
-  <true/>
+    <key>RunAtLoad</key>
+    <true/>
 
-  <key>StandardOutPath</key>
-  <string>/tmp/device.log</string>
-  <key>StandardErrorPath</key>
-  <string>/tmp/device.err</string>
+    <key>StandardOutPath</key>
+    <string>/tmp/device.log</string>
+    <key>StandardErrorPath</key>
+    <string>/tmp/device.err</string>
 </dict>
 </plist>
 `
